22b95560957c241e235c14255523120ac527d8fc
elissamunanira <munanira64@gmail.com>
2022-12-21 14:18:27 +0200

    responsiveness

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-var nameError = getElementById('nameError');
-var emailError = getElementById('emailError');
-var messageError = getElementById('messageError');
+var nameError = getElementById('name-error');
+var emailError = getElementById('email-error');
+var messageError = getElementById('message-error');
+
+function validateName(){
+    var name = document.getElementById('contact-name').value;
+    if(name.length == 0){
+        nameError.style.display = 'Name is required';
+        return false;
+    }
+}
