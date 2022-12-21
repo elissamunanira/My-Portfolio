8c7ed108e3de4805ac63b0f7fde8d58a84d42447
elissamunanira <munanira64@gmail.com>
2022-12-21 15:19:48 +0200

    responsiveness

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ var messageError = getElementById('message-error');
 function validateName(){
     var name = document.getElementById('contact-name').value;
     if(name.length == 0){
-        nameError.style.display = 'Name is required';
+        nameError.innerHTML = 'Name is required';
         return false;
     }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = 'Write a Full Name';
+        return false;
+    }
+    nameError.innerHTML = 'valid';
+    return true;
 }
