06ff23a44993ada3de4406de61287ea466497b5e
elissamunanira <munanira64@gmail.com>
2023-01-07 03:32:13 +0200

    responsiveness

--- index.js
+++ index.js
@@ -1,50 +1,40 @@
-
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 72,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.checkbtn');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('.nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+// ********** set date ************
+// select span
+const date = (document.getElementById(
+    "date"
+  ).innerHTML = new Date().getFullYear());
+  
+  // ********** nav toggle ************
+  // select button and links
+  const navBtn = document.getElementById("nav-toggle");
+  const links = document.getElementById("nav-links");
+  // add event listener
+  navBtn.addEventListener("click", () => {
+    links.classList.toggle("show-links");
+  });
+  
+  // ********** smooth scroll ************
+  // select links
+  const scrollLinks = document.querySelectorAll(".scroll-link");
+  scrollLinks.forEach(link => {
+    link.addEventListener("click", e => {
+      // prevent default
+      e.preventDefault();
+      links.classList.remove("show-links");
+  
+      const id = e.target.getAttribute("href").slice(1);
+      const element = document.getElementById(id);
+      //
+      let position = element.offsetTop - 62;
+  
+      window.scrollTo({
+        left: 0,
+        // top: element.offsetTop,
+        top: position,
+        behavior: "smooth"
+      });
     });
-
-});
-
+  });
 
 
 
